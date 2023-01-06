ce8a0588d9108f04e547bf08bdcf6e714897216b
NoahGX <114901822+NoahGX@users.noreply.github.com>
2023-01-05 20:04:49 -0800

    PlayerMovement

---
+++ DragonQuest2D/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float coolDownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && coolDownTimer > attackCooldown && playerMovement.canAttack())
+            Attack();
+
+        coolDownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        coolDownTimer = 0;
+    }
+}

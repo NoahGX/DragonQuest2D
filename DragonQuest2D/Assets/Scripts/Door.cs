b12606abaa0ecae418da535f7ce5002dd4c60f4c
NoahGX <114901822+NoahGX@users.noreply.github.com>
2023-01-08 13:38:58 -0800

    Camera

--- DragonQuest2D/Assets/Scripts/Door.cs
+++ DragonQuest2D/Assets/Scripts/Door.cs
@@ -6,16 +6,17 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private Transform previousRoom;
     [SerializeField] private Transform nextRoom;
+    [SerializeField] private CameraController cam;
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        if (collision.tag == "Player")
+        {
+            if (collision.transform.position.x < transform.position.x)
+                cam.MoveToNewRoom(nextRoom);
+            else
+                cam.MoveToNewRoom(previousRoom);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }

c7310135699e1387524c86fff92a118e53115fe1
NoahGX <114901822+NoahGX@users.noreply.github.com>
2023-01-09 17:01:29 -0800

    Update Health.cs

--- DragonQuest2D/Assets/Scripts/Health/Health.cs
+++ DragonQuest2D/Assets/Scripts/Health/Health.cs
@@ -1,17 +1,25 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -21,6 +29,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -37,5 +46,20 @@ public class Health : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
 
+    }
+
+    private void IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WatiForSeconds(1);
+            spriteRend.color = Color.white;
+            yield return new WatiForSeconds(1);
+        }
+
+        Physics2D.IgnoreLayerCollision(8, 9, false);
     }
 }

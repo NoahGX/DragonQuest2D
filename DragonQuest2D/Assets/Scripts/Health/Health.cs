cf99f30b27b17ee45d9f354021d1a6aa0d0e87cd
NoahGX <114901822+NoahGX@users.noreply.github.com>
2023-01-10 23:39:30 -0800

    Deactivate Death

--- DragonQuest2D/Assets/Scripts/Health/Health.cs
+++ DragonQuest2D/Assets/Scripts/Health/Health.cs
@@ -70,4 +70,9 @@ public class Health : MonoBehaviour
         Physics2D.IgnoreLayerCollision(10, 11, false);
         invulnerable = false;
     }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
 }

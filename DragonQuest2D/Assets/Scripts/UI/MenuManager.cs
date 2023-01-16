f6554fbb0a1ee895cb57c2df096ab6fa0a5cf314
NoahGX <114901822+NoahGX@users.noreply.github.com>
2023-01-16 15:00:31 -0800

    Changes

--- DragonQuest2D/Assets/Scripts/UI/MenuManager.cs
+++ DragonQuest2D/Assets/Scripts/UI/MenuManager.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private RectTransform arrow;
+    [SerializeField] private RectTransform[] buttons;
+    [SerializeField] private AudioClip changeSound;
+    [SerializeField] private AudioClip interactSound;
+    private int currentPosition;
+
+    private void Awake()
     {
-        
+        ChangePosition(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            ChangePosition(-1);
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            ChangePosition(1);
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetButtonDown("Submit"))
+            Interact();
+    }
+
+    public void ChangePosition(int _change)
+    {
+        currentPosition += _change;
+
+        if (_change != 0)
+            SoundManager.instance.PlaySound(changeSound);
+
+        if (currentPosition < 0)
+            currentPosition = buttons.Length - 1;
+        else if (currentPosition > buttons.Length - 1)
+            currentPosition = 0;
+
+        AssignPosition();
+    }
+
+    private void AssignPosition()
+    {
+        arrow.position = new Vector3(arrow.position.x, buttons[currentPosition].position.y);
+    }
+
+    private void Interact()
+    {
+        SoundManager.instance.PlaySound(interactSound);
+
+        if (currentPosition == 0)
+        {
+            //Start game
+            SceneManager.LoadScene(PlayerPrefs.GetInt("level", 1));
+        }
+        else if (currentPosition == 1)
+        {
+            //Open Settings
+        }
+        else if (currentPosition == 2)
+        {
+            //Open Credits
+        }
+        else if (currentPosition == 3)
+            Application.Quit();
     }
 }
--- DragonQuest2D/Assets/Scripts/UI/VolumeText.cs
+++ DragonQuest2D/Assets/Scripts/UI/VolumeText.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string volumeName;
+    [SerializeField] private string textIntro;
+    private Text txt;
+
+    private void Awake()
     {
-        
+        txt = GetComponent<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        UpdateVolume();
+    }
+
+    private void UpdateVolume()
+    {
+        float volumeValue = PlayerPrefs.GetFloat(volumeName) * 100;
+        txt.text = textIntro + volumeValue.ToString();
     }
 }
